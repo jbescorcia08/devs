98360e98f50a6356d74fad913bc81338827baa29
Jesus_Barrios <Jbescorcia@outlook.es>
2023-06-30 21:50:15 -0500

    prectica

--- python/datos_control/combate_pokemon.py
+++ python/datos_control/combate_pokemon.py
@@ -1,4 +1,12 @@
 from random import randint
+import os
+
+def limpiar_pantalla():
+    os.system("clear")
+
+
+VIDA_INICIAL_PYCACHU = 80
+VIDA_INICIAL_SQUIRTLE = 90
 
 vida_pycachu = 80
 vida_squirtle = 90
@@ -6,11 +14,19 @@ vida_squirtle = 90
 while vida_pycachu > 0 and vida_squirtle > 0:
     #Inicia el combate
     input("Enter para iniciar...\n")
-    print("La vida de Pycachu es de {} y la vida de Squirtle es {}\n".format(vida_pycachu,vida_squirtle))
+    limpiar_pantalla()
 
-    input("Enter para iniciar...")
+    print("La vida de Pycachu es de ")
+    porcentaje_pycachu = "#" * int(10 *  vida_pycachu/ VIDA_INICIAL_PYCACHU)
+    print("[{}] {}%".format(porcentaje_pycachu, vida_pycachu))
+    print("La vida de Squirtle es de ")
+    porcentaje_squirtle = "#" * int(10 * vida_squirtle / VIDA_INICIAL_SQUIRTLE)
+    print("[{}] {}%\n".format(porcentaje_squirtle, vida_squirtle))
+
+
+
     #Turno Pycachu
-    print("Turno Pycachu")
+    print("\nTurno Pycachu")
     ataque_pycachu = randint(1, 2)
     if ataque_pycachu == 1:
         #Bola voltio
@@ -21,15 +37,15 @@ while vida_pycachu > 0 and vida_squirtle > 0:
         print("Pycachu ataca con Onda de trueno")
         vida_squirtle -= 11
 
-    print("La vida de Pycachu es de {} y la vida de Squirtle es {}\n".format(vida_pycachu,vida_squirtle))
 
 
     input("Enter para iniciar...")
+    limpiar_pantalla()
     # Turno Squirtle
     print("Turno Squirtle")
     ataque_squirtle = None
-    while ataque_squirtle != "P" and ataque_squirtle != "A" and ataque_squirtle != "B":
-        ataque_squirtle = input("Que ataque deseas realizar: [P]lacaje. [A]gua. [B]urbuja\n")
+    while ataque_squirtle != "P" and ataque_squirtle != "A" and ataque_squirtle != "B" and ataque_squirtle != "N":
+        ataque_squirtle = input("Que ataque deseas realizar: [P]lacaje. [A]gua. [B]urbuja. [N]o\n")
     if ataque_squirtle == "P":
         vida_pycachu -= 12
         print("Ataca con placaje")
@@ -39,6 +55,22 @@ while vida_pycachu > 0 and vida_squirtle > 0:
     elif ataque_squirtle == "B":
         print("Ataca con Burbuja")
         vida_pycachu -= 9
+    elif ataque_squirtle == "N":
+        print("Paso")
+
+    if vida_pycachu < 0:
+        vida_pycachu = 0
+
+    if vida_squirtle < 0:
+        vida_squirtle = 0
+
+limpiar_pantalla()
+print("La vida de Pycachu es de ")
+porcentaje_pycachu = "#" * int(10 *  vida_pycachu/ VIDA_INICIAL_PYCACHU)
+print("[{}] {}%".format(porcentaje_pycachu, vida_pycachu))
+print("La vida de Squirtle es de ")
+porcentaje_squirtle = "#" * int(10 * vida_squirtle / VIDA_INICIAL_SQUIRTLE)
+print("[{}] {}%\n".format(porcentaje_squirtle, vida_squirtle))
 
 if vida_pycachu > vida_squirtle:
     print("Gana Pycachu")
